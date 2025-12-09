94d572c88d35d11abd565ee4a27beca6c187963f
matt-jg <matt.george.j@gmail.com>
2025-12-09 20:50:35 +1100

    finish ch3/l2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import number_of_words, count_each_character, create_sorted_list
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -6,7 +7,13 @@ def get_book_text(file_path):
     return file_contents
 
 def main():
-    book_path = "books/frankenstein.txt"
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+
+    book_path = sys.argv[1]
     raw_text = get_book_text(book_path)
 
     print("============ BOOKBOT ============")
